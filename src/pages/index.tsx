2652d26a6ae6e542602e2c39e5245956a2026adb
Bernardomennndes <83621700+Bernardomennndes@users.noreply.github.com>
2022-03-21 15:31:19 -0300

    Page: SignIn page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,38 @@
-export default function Home() {
+import { Button, Flex, Stack } from '@chakra-ui/react';
+import { Input } from '../components/Form/imput';
+
+export default function SignIn() {
   return (
-    <h1>DashGO</h1>
-  )
+    <Flex
+      w="100vw"
+      h="100vh"
+      align="center"
+      justify="center"
+    >
+      <Flex
+        as="form"
+        width="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDir="column"
+      >
+
+        <Stack spacing='4'>
+          <Input name="email" label="E-mail" type="email" />
+          <Input name="password" label="Password" type="password" />
+        </Stack>
+
+        <Button
+          type="submit"
+          mt="6"
+          colorScheme="pink"
+        >
+          Entrar
+        </Button>
+
+      </Flex>
+    </Flex>
+  );
 }
