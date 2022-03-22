0695288b11707691c3c1c9cf58200510686583d1
Bernardomennndes <83621700+Bernardomennndes@users.noreply.github.com>
2022-03-21 22:16:09 -0300

    Component: Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import { Flex, Input, Text, Icon } from '@chakra-ui/react';
-import { RiSearchLine } from 'react-icons/ri'
+import { Flex, Input, Text, Icon, HStack, Box, Avatar } from '@chakra-ui/react';
+import { RiAddLine, RiNotificationLine, RiSearchLine } from 'react-icons/ri'
 
 
 export function Header() {
@@ -41,7 +41,6 @@ export function Header() {
                     color="gray.50"
                     variant="unstyled"
                     px="4"
-                    mt="4"
                     placeholder='Search in plataform'
                     _placeholder={{
                         color: "gray.400"
@@ -49,6 +48,35 @@ export function Header() {
                 />
 
                 <Icon as={RiSearchLine} fontSize="20" />
+            </Flex>
+
+            <Flex
+                align="center"
+                ml="auto"
+            >
+
+                <HStack
+                    spacing="4"
+                    mx="8"
+                    pr="8"
+                    py="1"
+                    color="gray.300"
+                    borderRightWidth={1}
+                    borderColor="gray.700"
+                >
+                    <Icon as={RiNotificationLine} fontSize="20" />
+                    <Icon as={RiAddLine} fontSize="20" />
+                </HStack>
+
+                <Flex align="center">
+                    <Box mr="4" textAlign="right">
+                        <Text>Bernardo Mendes</Text>
+                        <Text color="gray.300" fontSize="small">bernardomennndes@outlook.com</Text>
+                    </Box>
+
+                    <Avatar size="md" name="Bernardo Mendes" src="https://github.com/bernardomennndes.png" />
+                </Flex>
+
             </Flex>
 
         </Flex>
