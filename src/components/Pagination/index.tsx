0901787883b5c5c7d32a0e167fcc93bbb3b34d9d
Bernardomennndes <83621700+Bernardomennndes@users.noreply.github.com>
2022-03-25 10:26:32 -0300

    Component: Pagination Components

---
+++ src/components/Pagination/index.tsx
@@ -0,0 +1,28 @@
+import { Box, Button, Stack } from "@chakra-ui/react";
+import { PaginationItem } from "./PaginationItem";
+
+export function Pagination() {
+    return (
+        <Stack
+            direction="row"
+            mt="8"
+            justify="space-between"
+            align="center"
+            spacing="6"
+        >
+            <Box>
+                <strong>0</strong> - <strong>10</strong> de <strong>100</strong>
+            </Box>
+
+            <Stack
+                direction="row"
+                spacing="2"
+            >
+                <PaginationItem pageNumber={1} isCurrent />
+                <PaginationItem pageNumber={2} />
+                <PaginationItem pageNumber={3} />
+                <PaginationItem pageNumber={4} />
+            </Stack>
+        </Stack>
+    );
+}
