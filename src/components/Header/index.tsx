0c4c7e396dccf8ac055d1553f151db48592f0f34
Bernardomennndes <83621700+Bernardomennndes@users.noreply.github.com>
2022-03-26 01:02:57 -0300

    Responsiveness: Sidebar

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,7 @@
-import { Flex, useBreakpointValue } from '@chakra-ui/react';
+import { Flex, Icon, IconButton, useBreakpointValue } from '@chakra-ui/react';
+import { useSidebarDrawer } from '../../contexts/SidebarDrawerContext';
+
+import { RiMenuLine } from 'react-icons/ri';
 
 import { Logo } from './Logo';
 import { NotificationsNav } from './NotificationsNav';
@@ -7,6 +10,9 @@ import { SearchBox } from './SearchBox';
 
 
 export function Header() {
+
+    const { onOpen } = useSidebarDrawer()
+
     const isWideVersion = useBreakpointValue({
         base: false,
         lg: true,
@@ -23,6 +29,19 @@ export function Header() {
             px="6"
             align="center"
         >
+
+            {!isWideVersion && (
+                <IconButton
+                    aria-label="Open navigation"
+                    icon={<Icon as={RiMenuLine} />}
+                    fontSize="24"
+                    variant="unstyled"
+                    onClick={onOpen}
+                    mr="2"
+                >
+
+                </IconButton>
+            )}
 
             <Logo />
 
