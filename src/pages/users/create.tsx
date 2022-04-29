37ff64950acb0d857abc0b3e2d36e38604359da0
Bernardomennndes <83621700+Bernardomennndes@users.noreply.github.com>
2022-04-28 22:35:04 -0300

    Functional: Data revalidation

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -1,13 +1,17 @@
 import { Box, Button, Divider, Flex, Heading, HStack, SimpleGrid, VStack } from "@chakra-ui/react";
 import Link from "next/link";
-import { Input } from "../../components/Form/input";
 
 import * as yup from 'yup';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { yupResolver } from "@hookform/resolvers/yup";
+import { useMutation } from 'react-query';
+import { api } from "../../services/api";
 
+import { Input } from "../../components/Form/input";
 import { Header } from "../../components/Header";
 import { Sidebar } from "../../components/Sidebar";
+import { queryClient } from "../../services/queryClient";
+import { useRouter } from "next/router";
 
 type CreateUserFormData = {
     name: string;
@@ -27,6 +31,23 @@ const createUserFormSchema = yup.object().shape({
 
 export default function CreateUser() {
 
+    const router = useRouter();
+
+    const createUser = useMutation(async (user: CreateUserFormData) => {
+        const response = await api.post('users', {
+            user: {
+                ...user,
+                created_at: new Date(),
+            }
+        })
+
+        return response.data.user;
+    },{
+        onSuccess: () => {
+            queryClient.invalidateQueries('users');
+        },
+    });
+
     const { register, handleSubmit, formState } = useForm({
         resolver: yupResolver(createUserFormSchema)
     })
@@ -34,9 +55,9 @@ export default function CreateUser() {
     const { errors } = formState;
 
     const handleCreateUser: SubmitHandler<CreateUserFormData> = async (values) => {
-        await new Promise(resolve => setTimeout(resolve, 2000))
+        await createUser.mutateAsync(values);
 
-        console.log(values)
+        router.push('/users');
     }
 
     return (
--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,4 +1,4 @@
-import { createServer, Factory, Model, Response } from 'miragejs';
+import { createServer, Factory, Model, Response, ActiveModelSerializer } from 'miragejs';
 import faker from 'faker';
 
 type User = {
@@ -9,6 +9,9 @@ type User = {
 
 export function makeServer() {
     const server = createServer({
+        serializers: {
+            application: ActiveModelSerializer,
+        },
         models: {
            user: Model.extend<Partial<User>>({}),
         },
@@ -43,7 +46,9 @@ export function makeServer() {
                 const pageStart = (Number(page) - 1) * Number(per_page);
                 const pageEnd = pageStart + Number(per_page);
 
-                const users = this.serialize(schema.all('user')).users.slice(pageStart, pageEnd);
+                const users = this.serialize(schema.all('user'))
+                    .users
+                    .slice(pageStart, pageEnd);
 
                 return new Response(
                     200,
